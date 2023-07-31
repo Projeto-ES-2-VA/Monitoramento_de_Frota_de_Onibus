7a56340efdeb8881767cca4bdab1fc9f3ad05d76
LuoLuann <luo.luann@gmail.com>
2023-07-31 19:53:57 -0300

    Adicionando validações aos campos e consertando form

--- app/models/onibus.rb
+++ app/models/onibus.rb
@@ -1,2 +1,9 @@
 class Onibus < ApplicationRecord
+  #has_many :rotum, dependent: :destroy
+
+  validates :placa, presence: true, length: {   is:7 }, format: { with: /\A[A-Z]{3}-\d{4}\z/ }
+  validates :chassi, presence: true, length: { minimum: 17, maximum: 18}, uniqueness: { case_sensitive: false }
+  validates :modelo, presence: true, length: { maximum: 250 }, uniqueness: false
+  validates :capacidade, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 24 }
+  validates :status, presence: true, inclusion: { in: [true, false] }
 end
