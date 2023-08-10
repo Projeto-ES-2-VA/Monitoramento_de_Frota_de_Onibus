d583fc2f816bf5acae67b79d50caf63ffe050d9c
LuoLuann <luo.luann@gmail.com>
2023-08-10 02:24:11 -0300

    adicionando mais validacoes ao model motorista

--- app/models/motoristum.rb
+++ app/models/motoristum.rb
@@ -1,12 +1,12 @@
 class Motoristum < ApplicationRecord
   include PgSearch::Model
 
-  validates :nome, presence: true
-  validates :cpf, presence: true, uniqueness: true, length: { is: 11 }
+  validates :nome, presence: true, length: { minimum: 10, maximum: 255 }, format: { with: /\A[a-zA-Z\s]+\z/ }
+  validates :cpf, presence: true, uniqueness: true, length: { is: 11 }, numericality: { only_integer: true }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :senha, presence: true, length: { minimum: 6 }
   validates :telefone, presence: true, format: { with: /\A\d{10,11}\z/, message: "deve conter 10 ou 11 dígitos numéricos" }
-  validates :cnh, presence: true, uniqueness: true, length: { is: 10 }
+  validates :cnh, presence: true, uniqueness: true, length: { is: 10 }, numericality: { only_integer: true }
 
   pg_search_scope :search,
                   against: [:cpf, :nome, :email],
