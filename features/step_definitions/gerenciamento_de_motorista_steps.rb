17666e76118b1d3725ab9bcbe3ca774345d64e15
DavyEugenio <davy.eugenio@hotmail.com>
2023-08-14 19:32:04 -0300

    Implementacao de teste Cucumber de atualizacao incorreta para gerenciamento de Motorista

--- features/step_definitions/gerenciamento_de_motorista_steps.rb
+++ features/step_definitions/gerenciamento_de_motorista_steps.rb
@@ -1,4 +1,3 @@
-#REGISTRAR COM DADOS VALIDOS
 Given('eu que sou um usuario do sistema') do
   visit "motorista"
   expect(page).to have_content("Motorista")
@@ -9,7 +8,7 @@ When('eu acesso a pagina de registro do motorista') do
   expect(page).to have_content("New motoristum")
 end
 
-When('o motorista de nome: {string}, cpf: {string}, email: {string}, senha: {string}, telefone: {string} , e cnh: {string}, existe') do |nome, cpf, email, senha, telefone, cnh|
+When('preencho do motorista o nome: {string}, cpf: {string}, email: {string}, senha: {string}, telefone: {string} , e cnh: {string}') do |nome, cpf, email, senha, telefone, cnh|
   fill_in "motoristum[nome]", with: nome
   fill_in "motoristum[cpf]", with: cpf
   fill_in "motoristum[email]", with: email
@@ -20,14 +19,6 @@ end
 
 When('clico no botao para cadastrar o motorista') do
   click_button "Create Motoristum"
-end
-
-Then('vejo uma mensagem de confirmacao de registro do motorista') do
-  expect(page).to have_content('Motoristum was successfully created.')
-end
-
-Then('vejo uma mensagem informando que nao e possivel cadastrar o motorista') do
-  expect(page).to have_content('16 errors prohibited this motoristum from being saved:')
 end
 
 #REGISTRAR COM DADOS INVALIDOS
@@ -43,19 +34,17 @@ Given('existe um motorista registrado com cpf {string}') do |cpf|
 end
 
 #REMOVER MOTORISTA
+Given('eu estou na pagina de listagem de motorista') do
+  visit 'motorista'
+end
 
-When('eu acesso a pagina de visualizacao do motorista com o cpf {string}') do |cpf|
-  visit "/motorista/#{Motoristum.find_by(cpf: cpf).id}"
+When('eu acesso um motorista em especifico') do
+  click_on 'Show this motoristum'
 end
 
 When('eu clico para excluir o motorista') do
   click_button 'Destroy this motoristum'
 end
-
-Then('aparece uma mensagem dizendo que o motorista foi excluido com sucesso') do
-  expect(page).to have_content('Motoristum was successfully destroyed.')
-end
-
 
 #EDITAR MOTORISTA
 Given('o motorista de nome: {string}, cpf: {string}, email: {string}, senha: {string}, telefone: {string} , e cnh: {string}') do |nome, cpf, email, senha, telefone, cnh|
@@ -67,26 +56,27 @@ Given('o motorista de nome: {string}, cpf: {string}, email: {string}, senha: {st
   fill_in "motoristum[cnh]", with: cnh
   end
 
-When('eu acesso a pagina de edicao do motorista com o cpf {string}') do |cpf|
-  visit "/motorista/#{Motoristum.find_by(cpf: cpf).id}/edit"
+When('eu clico para editar motorista') do
+  click_on 'Edit this motoristum'
 end
 
-And('eu atualizo o email do motorista para {string}') do |email|
+When('eu altero o campo email para {string}') do |email|
   fill_in 'Email', with: email
-end
-
-And('atualizo o telefone do motorista para {string}') do |telefone|
-  fill_in 'Telefone', with: telefone
 end
 
 When('clico para atualizar o motorista') do
   click_button 'Update Motoristum'
 end
 
-Then('vejo uma mensagem de falha ao tentar atualizar o telefone') do
-  expect(page).to have_content('1 error prohibited this motoristum from being saved')
+Then('aparece a mensagem {string}') do |mensagem|
+  expect(page).to have_content(mensagem)
 end
 
-Then('aparece uma mensagem dizendo que a motorista foi atualizado com sucesso') do
-  expect(page).to have_content('Motoristum was successfully updated.')
+Then('eu vejo os detalhes do motorista com o cpf: {string} e vejo os campos do mesmo') do |cpf|
+  motorista = Motoristum.find_by(cpf: cpf)
+  expect(page).to have_content("Nome: #{motorista.nome}")
+  expect(page).to have_content("Cpf: #{motorista.cpf}")
+  expect(page).to have_content("Email: #{motorista.email}")
+  expect(page).to have_content("Telefone: #{motorista.telefone}")
+  expect(page).to have_content("Cnh: #{motorista.cnh}")
 end
