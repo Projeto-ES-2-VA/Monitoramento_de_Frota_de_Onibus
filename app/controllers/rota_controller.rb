ee62bbf3a96f85f76e90c82053f61d7f61927fca
Luan Valentino Sampaio Marques <luan.valentino@ufrpe.br>
2023-08-15 23:48:20 -0300

    Codacy: rota_controller

--- app/controllers/rota_controller.rb
+++ app/controllers/rota_controller.rb
@@ -3,26 +3,22 @@ class RotaController < ApplicationController
 
   def home
   end
-  # GET /rota or /rota.json
+
   def index
     @rota = Rotum.all
   end
 
-  # GET /rota/1 or /rota/1.json
   def show
   end
 
-  # GET /rota/new
   def new
     @rotum = Rotum.new
     list_onibus_and_motorista
   end
 
-  # GET /rota/1/edit
   def edit
   end
 
-  # POST /rota or /rota.json
   def create
     @rotum = Rotum.new(rotum_params)
     respond_to do |format|
@@ -30,14 +26,12 @@ class RotaController < ApplicationController
         format.html { redirect_to rotum_url(@rotum), notice: "Rotum was successfully created." }
         format.json { render :show, status: :created, location: @rotum }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render_error_and_list_onibus_and_motorista }
         format.json { render json: @rotum.errors, status: :unprocessable_entity }
-        list_onibus_and_motorista
       end
     end
   end
 
-  # PATCH/PUT /rota/1 or /rota/1.json
   def update
     respond_to do |format|
       if @rotum.update(rotum_params)
@@ -50,10 +44,8 @@ class RotaController < ApplicationController
     end
   end
 
-  # DELETE /rota/1 or /rota/1.json
   def destroy
     @rotum.destroy
-
     respond_to do |format|
       format.html { redirect_to rota_url, notice: "Rotum was successfully destroyed." }
       format.json { head :no_content }
@@ -72,13 +64,16 @@ class RotaController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_rotum
       @rotum = Rotum.find_by(id: params[:id])
       list_onibus_and_motorista
     end
 
-    # Only allow a list of trusted parameters through.
+  def render_error_and_list_onibus_and_motorista
+    render :new, status: :unprocessable_entity
+    list_onibus_and_motorista
+  end
+
     def rotum_params
       params.require(:rotum).permit(:nome, :valor, :distancia, :duracao, :inicio, :fim, :origem, :destino, :onibus_id, :motoristum_id)
     end
