222b6ec2fbe453b49bed54feee8b65c41d2a960b
LuoLuann <luo.luann@gmail.com>
2023-08-21 18:21:07 -0300

    tirando confirmacao por email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :validatable
 
   # database_authenticatable: users can authenticate themselves with a login and passwords
   # their sncrypted password will be stored in your database
